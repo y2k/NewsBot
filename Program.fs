5c0e4ee2a24353a4c7c5d81ee0f2bae778c2e7af
y2k <itwisterlx@gmail.com>
2019-09-04 18:30:49 +0300

    add db

--- Program.fs
+++ Program.fs
@@ -4,26 +4,80 @@ open System
 
 let inline (^) f x = f x
 
+type Message = { text : string; url : Uri }
+type Database = { newMessages : Message list; sended : Message list }
+type RawMessage = RawMessage of string
+
+module Store =
+    let private store = ref { newMessages = []; sended = [] }
+
+    let update f = async { store := f !store }
+    let dispatch f = async {
+        let (db, x) = f !store
+        store := db
+        return x }
+
 module Domain =
     open System.Text.RegularExpressions
 
-    type RawMessage = RawMessage of string
-
-    let findUrl (text : string) : Uri option = 
+    let private findUrl (text : string) : Uri option =
         Regex.Match(text, "https://[^\" ]+")
         |> fun x -> if x.Success then Some x.Value else None
         |> Option.map Uri
 
-    let parse (updates : RawMessage list) =
+    let mkMessage (updates : RawMessage list) =
         updates
         |> List.choose ^ fun (RawMessage x) -> findUrl x |> Option.map ^ fun y -> x, y
-        |> List.map ^ fun (x, u) -> sprintf "%s" x
-        |> List.fold (+) ""
+        |> List.groupBy ^ fun (_, x) -> x
+        |> List.map ^ fun (url, xs) -> fst xs.[0], url
+        |> List.map ^ fun (x, _) -> sprintf "%s" x
+        |> List.fold (sprintf "%s- %s\n") ""
+        |> fun x -> if String.IsNullOrEmpty x then None else Some x
 
-open Telegram.Bot
+    let update db message =
+        match findUrl message with
+        | Some url ->
+            let exists =
+                db.newMessages
+                |> List.append db.sended
+                |> List.exists ^ fun x -> x.url = url
+            if exists then db
+            else { db with newMessages = { text = message; url = url } :: db.newMessages }
+        | None -> db
+
+    let mkMesssage db =
+        let message =
+            db.newMessages
+            |> List.map ^ fun x -> RawMessage x.text
+            |> mkMessage
+        { db with newMessages = []; sended = db.newMessages @ db.sended }, message
+
+open Telegram.Bot.Types
 
 [<EntryPoint>]
-let main argv =
-    // use client = new Net.Http.HttpClient()
-    // let client = TelegramBotClient(argv.[0], client)
+let main _ =
+    let listenSlack tag =
+        SlackParser.startReadingUpdates
+            tag
+            ^ fun message ->
+                Store.update ^ fun db -> Domain.update db message
+
+    let sendToTelegram token = async {
+        while true do
+            let! optMsg = Store.dispatch Domain.mkMesssage
+
+            match optMsg with
+            | None -> ()
+            | Some msg ->
+                let bot = Telegram.Bot.TelegramBotClient token
+                let! _ = bot.SendTextMessageAsync(ChatId.op_Implicit "TODO", msg) |> Async.AwaitTask
+                ()
+
+            do! Async.Sleep 5000 }
+
+    Async.Parallel
+        [ sendToTelegram "TODO"
+          listenSlack "compose"
+          listenSlack "feed" ]
+    |> Async.RunSynchronously |> ignore
     0
--- SlackParser.fs
+++ SlackParser.fs
@@ -3,73 +3,74 @@ module SlackParser
 let private (>>-) a f = async.Bind(a, fun x -> async.Return <| f x)
 let private (>>-!) t f = (Async.AwaitTask t) >>- f
 
-module Slack =
+module private Slack =
     open System
     open SlackAPI
 
-    type SlackChannel = 
+    type SlackChannel =
         { channel_id : string
           name : string
           num_members : int
           purpose : string }
 
-    type Message = 
+    type Message =
         { text : string
           user : string
-          ts   : string }
+          ts : string }
 
     let private userId = lazy (Environment.GetEnvironmentVariable "SLACK_API_USERID")
     let private password = lazy (Environment.GetEnvironmentVariable "SLACK_API_PASSWORD")
     let private cachedClient : SlackTaskClient option ref = ref None
 
-    let private makeClient =
-        async {
-            match !cachedClient with
-            | Some client -> return client
-            | None ->
-                let! response =
-                    SlackClientHelpers().AuthSigninAsync(userId.Value, "T09229ZC6", password.Value)
-                    |> Async.AwaitTask
-                let x = SlackTaskClient response.token
-                cachedClient := Some x
-                return x
-        }
+    let private makeClient = async {
+        match !cachedClient with
+        | Some client -> return client
+        | None ->
+            let! response =
+                SlackClientHelpers().AuthSigninAsync(userId.Value, "T09229ZC6", password.Value)
+                |> Async.AwaitTask
+            let x = SlackTaskClient response.token
+            cachedClient := Some x
+            return x }
 
-    let getSlackChannels =
-        async {
-            let! client = makeClient
-            let! channels = client.GetChannelListAsync() |> Async.AwaitTask
+    let getSlackChannels = async {
+        let! client = makeClient
+        let! channels = client.GetChannelListAsync() |> Async.AwaitTask
 
-            return
-                channels.channels
-                |> Array.map (fun x -> { channel_id = x.id
-                                         name = x.name
-                                         num_members = x.num_members
-                                         purpose = "" })
-                |> Array.toList
-        }
+        return
+            channels.channels
+            |> Array.map (fun x -> { channel_id = x.id
+                                     name = x.name
+                                     num_members = x.num_members
+                                     purpose = "" })
+            |> Array.toList }
 
-    let getSlackMessages name =
-        async {
-            let! client = makeClient
-            let! channels = client.GetChannelListAsync() |> Async.AwaitTask
+    let getSlackMessages name = async {
+        let! client = makeClient
+        let! channels = client.GetChannelListAsync() |> Async.AwaitTask
 
-            let id =
-                channels.channels
-                |> Array.tryFind (fun x -> x.name = name)
+        let id =
+            channels.channels
+            |> Array.tryFind (fun x -> x.name = name)
 
-            let! history =
-                match id with
-                | None -> async.Return [||]
-                | Some id ->
-                    client.GetChannelHistoryAsync(id, count = Nullable 10)
-                    >>-! fun x -> x.messages
+        let! history =
+            match id with
+            | None -> async.Return [||]
+            | Some id ->
+                client.GetChannelHistoryAsync(id, count = Nullable 10)
+                >>-! fun x -> x.messages
 
-            return
-                history
-                |> Array.map (fun x ->
-                    { text = x.text
-                      user = x.username
-                      ts = string x.ts.Ticks })
-                |> Array.toList
-        }
+        return
+            history
+            |> Array.map (fun x ->
+                { text = x.text
+                  user = x.username
+                  ts = string x.ts.Ticks })
+            |> Array.toList }
+
+let startReadingUpdates name f = async {
+    while true do
+        let! messages = Slack.getSlackMessages name
+        for m in messages do
+            do! f m.text 
+        do! Async.Sleep 30_000 }
