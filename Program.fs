301be2ef0575cf775a27a1b45413cc9d086e96db
y2k <itwisterlx@gmail.com>
2019-11-18 18:01:34 +0300

    refactoring

--- Program.fs
+++ Program.fs
@@ -1,20 +1,43 @@
-﻿module Application
+﻿open System
 
-open System
-open SlackParser
+let inline (^) f x = f x
+let inline (>>-) a f = async.Bind(a, fun x -> async.Return ^ f x)
+let inline (>>-!) t f = (Async.AwaitTask t) >>- f
+
+module SlackParser =
+    open SlackAPI
+
+    type SlackConfig =
+        { userId : string
+          password : string }
+
+    let private getSlackMessages (client : SlackTaskClient) name from = async {
+        let! channels = client.GetChannelListAsync() |> Async.AwaitTask
+        let channel = channels.channels |> Array.tryFind ^ fun channel -> channel.name = name
+        return!
+            match channel with
+            | None -> async.Return [||]
+            | Some id ->
+                client.GetChannelHistoryAsync(id, count = Nullable 30, oldest = Nullable from)
+                >>-! fun x -> x.messages }
+
+    let startReadingUpdates cfg name f = async {
+        let! client =
+            SlackClientHelpers().AuthSigninAsync(cfg.userId, "T09229ZC6", cfg.password)
+            >>-! fun response -> SlackTaskClient response.token
+
+        let from = ref DateTime.Now
+
+        while true do
+            let! messages = getSlackMessages client name !from
+            from := messages |> Array.map (fun x -> x.ts) |> Array.max
+            for m in messages do
+                do f m.text
+            do! Async.Sleep 30_000 }
 
 type Message = { text : string; url : Uri }
 type Database = { newMessages : Message list; sended : Message list }
 type RawMessage = RawMessage of string
-
-module Store =
-    let private store = ref { newMessages = []; sended = [] }
-
-    let dispatch f = async {
-        let (db, x) = f !store
-        store := db
-        return x }
-    let update f = dispatch ^ fun db -> f db, ()
 
 module Domain =
     open System.Text.RegularExpressions
@@ -35,7 +58,7 @@ module Domain =
             else { db with newMessages = { text = message; url = url } :: db.newMessages }
         | None -> db
 
-    let private mkMessage (updates : RawMessage list) =
+    let private mkMessage' (updates : RawMessage list) =
         updates
         |> List.choose ^ fun (RawMessage x) -> findUrl x |> Option.map ^ fun y -> x, y
         |> List.groupBy ^ fun (_, x) -> x
@@ -44,31 +67,44 @@ module Domain =
         |> List.fold (sprintf "%s- %s\n") ""
         |> fun x -> if String.IsNullOrEmpty x then None else Some x
 
-    let mkMesssage db =
+    let mkMessage db =
         let message =
             db.newMessages
             |> List.map ^ fun x -> RawMessage x.text
-            |> mkMessage
+            |> mkMessage'
         { db with newMessages = []; sended = db.newMessages @ db.sended }, message
+
+module Store =
+    open System.Threading
+    
+    let private store = ref { newMessages = []; sended = [] }
+
+    let rec dispatch f = 
+        let currentValue = !store
+        let (newValue, result') = f currentValue
+        let result = Interlocked.CompareExchange(store, newValue, currentValue)
+        if obj.ReferenceEquals(result, currentValue) then result'
+        else Thread.SpinWait 20; dispatch f
+    let update f = dispatch ^ fun db -> f db, ()
 
 open Telegram.Bot.Types
 
 [<EntryPoint>]
 let main _ =
-    let cfg : {| slack : SlackConfig; token : string; chatId : string |} =
+    let cfg : {| slack : SlackParser.SlackConfig; token : string; chatId : string |} =
         {| slack = { userId = Environment.GetEnvironmentVariable "SLACK_API_USERID"
                      password = Environment.GetEnvironmentVariable "SLACK_API_PASSWORD" }
            token = Environment.GetEnvironmentVariable "TELEGRAM_TOKEN"
            chatId = Environment.GetEnvironmentVariable "TELEGRAM_CHAT_ID" |}
 
     let listenSlack tag =
-        startReadingUpdates cfg.slack tag
+        SlackParser.startReadingUpdates cfg.slack tag
             ^ fun message ->
                 Store.update ^ fun db -> Domain.update db message
 
     let sendToTelegram = async {
         while true do
-            let! optMsg = Store.dispatch Domain.mkMesssage
+            let optMsg = Store.dispatch Domain.mkMessage
 
             do! optMsg
                 |> Option.map ^ fun msg ->
